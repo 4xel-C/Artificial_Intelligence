97f5299d8d59e128e4357730948ef8dc4b60f605
Axel Cano <axel-cano@live.fr>
2024-11-03 20:50:34 +0100

    Adding new nim script to implement Q-learning algorithm for reinforcement learning

--- learning/banknotes/banknotes1.py
+++ learning/banknotes/banknotes1.py
@@ -6,6 +6,8 @@ import csv
 from sklearn import svm
 from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 
 model = Perceptron()
 # model = svm.SVC()
